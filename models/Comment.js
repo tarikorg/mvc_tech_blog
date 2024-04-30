e5452f996357d2cc0b524b1e14fc9ec3ed02ffbd
tarikorg <myku1771@gmail.com>
2024-04-29 22:31:08 -0400

    commit models-Commentjs written

--- models/Comment.js
+++ models/Comment.js
@@ -1,2 +1,20 @@
 const { Model, DataTypes } = require('sequelize')
 const client = require('../config/config')// connection to sequelize
+
+class Comment extends Model {}
+
+
+Comment.init(
+    {
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    },
+    {
+        sequelize: client,
+        modelName: 'comment'
+    }
+)
+
+module.exports = Comment
